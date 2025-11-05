e78a221a10e37a96209062669237a5870d8415a0
Praju0745 <prajwal2003patel@gmail.com>
2025-11-05 20:34:28 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name='scrapper',
     version='0.0.1',
-    author='pwskills',
-    author_email='md.a@pw.live',
+    author='prajwal patel',
+    author_email='prajuofficialpatel@gmail.com',
     packages=find_packages(),
     install_requires=[]
 )
