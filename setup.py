f79d5af5c37b1ca05a63dc39b3abaa95b3006538
Praju0745 <prajwal2003patel@gmail.com>
2025-11-05 20:37:47 +0530

    modified setup

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='scrapper',
     version='0.0.1',
-    author='prajwal patel',
+    author='Prajwal Patel A N',
     author_email='prajuofficialpatel@gmail.com',
     packages=find_packages(),
     install_requires=[]
